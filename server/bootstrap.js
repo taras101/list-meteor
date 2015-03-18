b73620409442f1bc7fa7c719ff6b2112dd48f7d3
taras101 <taras101@gmail.com>
2015-03-18 15:02:02 -0400

    removed some app startup data

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,37 +2,12 @@
 Meteor.startup(function () {
   if (Lists.find().count() === 0) {
     var data = [
-      {name: "Meteor Principles",
-       items: ["Data on the Wire",
-         "One Language",
-         "Database Everywhere",
-         "Latency Compensation",
-         "Full Stack Reactivity",
-         "Embrace the Ecosystem",
-         "Simplicity Equals Productivity"
+      {name: "Your first list",
+       items: ["Something",
+         "Something else",
+         "You get the idea"
        ]
       },
-      {name: "Languages",
-       items: ["Lisp",
-         "C",
-         "C++",
-         "Python",
-         "Ruby",
-         "JavaScript",
-         "Scala",
-         "Erlang",
-         "6502 Assembly"
-         ]
-      },
-      {name: "Favorite Scientists",
-       items: ["Ada Lovelace",
-         "Grace Hopper",
-         "Marie Curie",
-         "Carl Friedrich Gauss",
-         "Nikola Tesla",
-         "Claude Shannon"
-       ]
-      }
     ];
 
     var timestamp = (new Date()).getTime();
